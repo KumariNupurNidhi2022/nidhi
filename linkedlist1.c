0c7cd4850ada2c39ca091ef35b0b5c155b12b365
KumariNupurNidhi2022 <120093798+KumariNupurNidhi2022@users.noreply.github.com>
2023-05-05 12:16:24 +0530

    Add files via upload

---
+++ linkedlist1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int arr[5];//globalscope
+void read()
+{
+	int i;
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+}
+
+int main()
+{
+	read();
+	int i;
+	for(i=0;i<5;i++)
+	{
+		printf(" %d",arr[i]);
+	}
+	return 0;
+}
